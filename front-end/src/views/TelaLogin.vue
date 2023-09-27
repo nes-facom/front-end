d511c7e0fa8546adde74c0e9b01cc97f5137e918
Vitor Cassol <80921175+vitorhcassol@users.noreply.github.com>
2023-09-27 14:51:11 -0400

    style: estilização da tela login finalizada

--- front-end/src/views/TelaLogin.vue
+++ front-end/src/views/TelaLogin.vue
@@ -1,50 +1,44 @@
 <template>
     <div
     id="background">
-        <v-card
-            outlined
-            height="90vh"
-            width="25vw"
-            min-width="45rem"
-            id="card"
-        >
-            <v-form
-                id="form"
-            >
-                <img src="@/components/icons/Logo.png" alt="Um livro fechado com o nome LibriX à esquerda.">
-
-                <div>
-                    <v-text-field
-                        label="CPF"
-                        outlined
-                        prepend-inner-icon="mdi-account"
-                        hint="Utilize apenas números"
-                        counter="11"
-                        class="font-style-body-large"
-                    ></v-text-field>
-                    <v-text-field
-                        prepend-inner-icon="mdi-key"
-                        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                        :type="showPassword ? 'text' : 'password'"
-                        label="Senha"
-                        outlined
-                        hint="Pelo menos 8 caracteres"
-                        @click:append="showPassword = !showPassword"
-                        class="font-style-body-large"
-                    ></v-text-field>
-                </div>
-
-                <v-btn
-                    id="button"
-                    rounded
-                    class="font-size-label-large"
+        <div id="wrapper">
+            <v-card
+                outlined
+                height="90vh"
+                width="25vw"
+                min-width="45rem"
+                id="card"
                 >
-                    Entrar
-                </v-btn>
-            </v-form>
-
-            <a href="#">Cadastrar bibliotecário</a>
-        </v-card>
+                <v-form
+                    id="form"
+                >
+                    <img src="@/components/icons/Logo.png" alt="Um livro fechado com o nome LibriX à esquerda.">
+                    <div>
+                        <v-text-field
+                            label="CPF"
+                            outlined
+                            prepend-inner-icon="mdi-account"
+                        ></v-text-field>
+                        <v-text-field
+                            prepend-inner-icon="mdi-key"
+                            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                            :type="showPassword ? 'text' : 'password'"
+                            label="Senha"
+                            outlined
+                            @click:append="showPassword = !showPassword"
+                        ></v-text-field>
+                    </div>
+                    <v-btn
+                        id="button"
+                        rounded
+                        elevation="0"
+                    >
+                        Entrar
+                    </v-btn>
+                </v-form>
+                <a href="#">Cadastrar bibliotecário</a>
+            </v-card>
+        </div>
     </div>
 </template>
 
@@ -66,10 +60,8 @@ export default {
 
 #background {
     display: flex;
-    justify-content: end;
+    justify-content: center;
     align-items: center;
-
-    padding-inline: 2rem;
 
     height: 100vh;
 
@@ -77,6 +69,18 @@ export default {
     background-size: cover;
     background-repeat: no-repeat;
     background-attachment: fixed;
+}
+
+#wrapper {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    
+    height: 100vh;
+    width: 100vw;
+    max-width: 128rem;
+
+    padding-inline: 2rem;
 }
 
 #card {
@@ -97,17 +101,48 @@ export default {
     align-items: center;
 
     gap: 8rem;
+
+    ::v-deep .v-input__slot {
+        gap: 1.5rem;
+    }
+
+    ::v-deep .v-label {
+        font: var(--body-large);
+        color: var(--on-surface-variant);
+    }
+
+    ::v-deep .v-icon {
+        color: var(--primary);
+    }
+
+    ::v-deep .v-input--is-focused:first-child legend {
+        width: 7.5rem !important;
+    }
+
+    ::v-deep .v-input--is-label-active:first-child legend {
+        width: 7.5rem !important;
+    }
+
+    ::v-deep .v-input--is-focused:last-child legend {
+        width: 8.5rem !important;
+    }
+
+    ::v-deep .v-input--is-label-active:last-child legend {
+        width: 8.5rem !important;
+    }
 }
 
 #button {
-    background-color: var(--primary-color);
-    color: var(--white);
-    width: fit-content;
+    background-color: var(--primary);
+    color: var(--on-primary);
+    width: fit-content; 
 }
 
 a {
-    color: var(--primary-color);
+    font: var(--label-large);
+    color: var(--primary);
     text-decoration: none;
 }
+
 
 </style>
